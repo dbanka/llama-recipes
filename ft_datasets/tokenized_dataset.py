79a80e370dc3f0e91b676f3fcd612824966cf38e
Deepak Banka <deepak.banka@phenompeople.com>
2023-08-21 08:46:05 +0530

    changed streaming dataset to return list

--- ft_datasets/tokenized_dataset.py
+++ ft_datasets/tokenized_dataset.py
@@ -1,13 +1,31 @@
 from streaming import StreamingDataset
+from typing import Callable, Any, Tuple
+
+
+class TokenisedDataset(StreamingDataset):
+    def __init__(self,
+                 remote: str,
+                 local: str,
+                 shuffle: bool,
+                 ) -> None:
+        super().__init__(local=local, remote=remote, shuffle=shuffle)
+
+    def __getitem__(self, idx: int) -> Any:
+        obj = super().__getitem__(idx)
+        obj['input_ids'] = obj['input_ids'].tolist()
+        obj['attention_mask'] = obj['attention_mask'].tolist()
+        obj['labels'] = obj['labels'].tolist()
+        return obj
 
 
 def get_tokenized_dataset(dataset_config, tokenizer, split="train"):
     # Create streaming dataset
     if split == "train":
-        dataset = StreamingDataset(local=dataset_config.data_path + "/train",
+        dataset = TokenisedDataset(local=dataset_config.data_path + "/train",
                                    remote=dataset_config.remote_data_path + "/train", shuffle=True)
+
     else:
-        dataset = StreamingDataset(local=dataset_config.data_path + "/test",
+        dataset = TokenisedDataset(local=dataset_config.data_path + "/test",
                                    remote=dataset_config.remote_data_path + "/test", shuffle=True)
 
     return dataset
