fd3b89884eec88beeee6e7d646ef310181f0cc3b
Deepak Banka <deepak.banka@phenompeople.com>
2023-08-21 20:04:35 +0530

    shuffle algo in streaming dataset

--- ft_datasets/tokenized_dataset.py
+++ ft_datasets/tokenized_dataset.py
@@ -7,8 +7,11 @@ class TokenisedDataset(StreamingDataset):
                  remote: str,
                  local: str,
                  shuffle: bool,
+                 shuffle_seed:int,
+                 shuffle_algo:str
                  ) -> None:
-        super().__init__(local=local, remote=remote, shuffle=shuffle)
+        super().__init__(local=local, remote=remote, shuffle=shuffle, shuffle_seed=shuffle_seed,
+                         shuffle_algo= shuffle_algo)
 
     def __getitem__(self, idx: int) -> Any:
         obj = super().__getitem__(idx)
@@ -22,10 +25,12 @@ def get_tokenized_dataset(dataset_config, tokenizer, split="train"):
     # Create streaming dataset
     if split == "train":
         dataset = TokenisedDataset(local=dataset_config.data_path + "/train",
-                                   remote=dataset_config.remote_data_path + "/train", shuffle=True)
+                                   remote=dataset_config.remote_data_path + "/train", shuffle=True,
+                                   shuffle_seed=42, shuffle_algo= "py1s")
 
     else:
         dataset = TokenisedDataset(local=dataset_config.data_path + "/test",
-                                   remote=dataset_config.remote_data_path + "/test", shuffle=True)
+                                   remote=dataset_config.remote_data_path + "/test", shuffle=True,
+                                   shuffle_seed=42, shuffle_algo="py1s")
 
     return dataset
