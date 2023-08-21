5a2f56aa5b6954ed6924b8a59efd8728815a3427
Harshit Ajmani <harshit.ajmani@phenompeople.com>
2023-08-21 19:32:48 +0530

    Jira No: <Please add link of Jira> Is Bug: Yes/No Cause: <if bug> Comment: (Solution or Update over the task) Reviewer: <Name of the reviewer>
    Note: <Any additional information like design/limitation/tradeoff etc..>

--- ft_datasets/tokenized_dataset.py
+++ ft_datasets/tokenized_dataset.py
@@ -3,14 +3,24 @@ from typing import Callable, Any, Tuple
 
 
 class TokenisedDataset(StreamingDataset):
+    dummy_sample = {
+        'input_ids': [],
+        'attention_mask': [],
+        'labels': []
+    }
     def __init__(self,
                  remote: str,
                  local: str,
                  shuffle: bool,
+                 shuffle_seed: int,
+                 offset: int = 0
                  ) -> None:
-        super().__init__(local=local, remote=remote, shuffle=shuffle)
+        super().__init__(local=local, remote=remote, shuffle=shuffle, shuffle_seed=shuffle_seed)
+        self.offset = offset
 
     def __getitem__(self, idx: int) -> Any:
+        if idx < self.offset:
+            return self.dummy_sample 
         obj = super().__getitem__(idx)
         obj['input_ids'] = obj['input_ids'].tolist()
         obj['attention_mask'] = obj['attention_mask'].tolist()
@@ -22,10 +32,14 @@ def get_tokenized_dataset(dataset_config, tokenizer, split="train"):
     # Create streaming dataset
     if split == "train":
         dataset = TokenisedDataset(local=dataset_config.data_path + "/train",
-                                   remote=dataset_config.remote_data_path + "/train", shuffle=True)
+                                   remote=dataset_config.remote_data_path + "/train", 
+                                   shuffle=True,
+                                   shuffle_seed=42)
 
     else:
         dataset = TokenisedDataset(local=dataset_config.data_path + "/test",
-                                   remote=dataset_config.remote_data_path + "/test", shuffle=True)
+                                   remote=dataset_config.remote_data_path + "/test", 
+                                   shuffle=True,
+                                   shuffle_seed=42)
 
     return dataset
