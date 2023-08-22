9c2d1072356596385d2df26b6e56d33a4ed319d1
Harshit Ajmani <harshit.ajmani@phenompeople.com>
2023-08-22 20:00:44 +0530

    fixed args

--- ft_datasets/tokenized_dataset.py
+++ ft_datasets/tokenized_dataset.py
@@ -7,11 +7,11 @@ class TokenisedDataset(StreamingDataset):
                  remote: str,
                  local: str,
                  shuffle: bool,
-                 shuffle_seed:int,
-                 shuffle_algo:str,
+                 shuffle_seed:int = 42,
+                 shuffle_algo:str = "py1s",
                  ) -> None:
         super().__init__(local=local, remote=remote, shuffle=shuffle, shuffle_seed=shuffle_seed,
-                         shuffle_algo= shuffle_algo)
+                         shuffle_algo=shuffle_algo)
 
 
     def __getitem__(self, idx: int) -> Any:
@@ -26,8 +26,7 @@ def get_tokenized_dataset(dataset_config, tokenizer, split="train"):
     # Create streaming dataset
     if split == "train":
         dataset = TokenisedDataset(local=dataset_config.data_path + "/train",
-                                   remote=dataset_config.remote_data_path + "/train", shuffle=True,
-                                   shuffle_seed=42, shuffle_algo= "py1s")
+                                   remote=dataset_config.remote_data_path + "/train", shuffle=True)
 
     else:
         dataset = TokenisedDataset(local=dataset_config.data_path + "/test",
