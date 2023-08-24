7e21e49d5535c87f1893a5460ae22b727e86b512
Harshit Ajmani <harshit.ajmani@phenompeople.com>
2023-08-24 19:03:44 +0530

    batch size

--- configs/training.py
+++ configs/training.py
@@ -23,7 +23,7 @@ class train_config:
     mixed_precision: bool=False
     val_batch_size: int=1
     dataset = "tokenized_dataset"
-    micro_batch_size: int=2
+    micro_batch_size: int=1
     peft_method: str = "lora" # None , llama_adapter, prefix
     use_peft: bool=False
     output_dir: str = "/opt/ml/model"
