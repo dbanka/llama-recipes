7db7ffd71572a5619f88efbf76f5fe4a9b0d0857
Harshit Ajmani <harshit.ajmani@phenompeople.com>
2023-08-25 12:15:04 +0530

    rank 0 logs

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -179,12 +179,12 @@ def train(model, train_dataloader, eval_dataloader, tokenizer, optimizer, lr_sch
         train_loss.append(train_epoch_loss)
         
         if train_config.enable_fsdp:
-            # if rank==0:
-            print(f"During training - Rank - {rank} - Max CUDA memory allocated was {memtrace.peak} GB")
-            print(f"During training - Rank - {rank} - Max CUDA memory reserved was {memtrace.max_reserved} GB")
-            print(f"During training - Rank - {rank} - Peak active CUDA memory was {memtrace.peak_active_gb} GB")
-            print(f"During training - Rank - {rank} - Cuda Malloc retires : {memtrace.cuda_malloc_retires}")
-            print(f"During training - Rank - {rank} - CPU Total Peak Memory consumed during the train (max): {memtrace.cpu_peaked + memtrace.cpu_begin} GB")
+            if rank==0:
+                print(f"During training - Rank - {rank} - Max CUDA memory allocated was {memtrace.peak} GB")
+                print(f"During training - Rank - {rank} - Max CUDA memory reserved was {memtrace.max_reserved} GB")
+                print(f"During training - Rank - {rank} - Peak active CUDA memory was {memtrace.peak_active_gb} GB")
+                print(f"During training - Rank - {rank} - Cuda Malloc retires : {memtrace.cuda_malloc_retires}")
+                print(f"During training - Rank - {rank} - CPU Total Peak Memory consumed during the train (max): {memtrace.cpu_peaked + memtrace.cpu_begin} GB")
         else:
             print(f"Max CUDA memory allocated was {memtrace.peak} GB")
             print(f"Max CUDA memory reserved was {memtrace.max_reserved} GB")
