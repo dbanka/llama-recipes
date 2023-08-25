930ad73595105ef8077020eaa547d1f357b0a047
Harshit Ajmani <harshit.ajmani@phenompeople.com>
2023-08-25 17:02:18 +0530

    changed batch size

--- configs/training.py
+++ configs/training.py
@@ -12,7 +12,7 @@ class train_config:
     enable_fsdp: bool=True
     low_cpu_fsdp: bool=False
     run_validation: bool=False
-    batch_size_training: int=6
+    batch_size_training: int= 8
     num_epochs: int=1
     num_workers_dataloader: int=1
     lr: float=1e-4
@@ -23,7 +23,7 @@ class train_config:
     mixed_precision: bool=False
     val_batch_size: int=1
     dataset = "tokenized_dataset"
-    gradient_accumulation_steps: int=18
+    gradient_accumulation_steps: int=24
     peft_method: str = "lora" # None , llama_adapter, prefix
     use_peft: bool=False
     output_dir: str = "/opt/ml/model"
