a9f3649816b3a1774618c212ec599852bdae7e39
Harshit Ajmani <harshit.ajmani@phenompeople.com>
2023-08-31 14:26:59 +0530

    added new data streams

--- configs/datasets.py
+++ configs/datasets.py
@@ -48,5 +48,6 @@ class tokenized_dataset:
         's3://716533421362-spx-data/phenom-llm-data/streaming-data/resume-freetext',
         's3://716533421362-spx-data/phenom-llm-data/streaming-data/resume-html',
         's3://716533421362-spx-data/phenom-llm-data/streaming-data/fitscore-data',
+        's3://716533421362-spx-data/phenom-llm-data/streaming-data/skill-title-mapping',
         # 's3://716533421362-spx-data/phenom-llm-data/streaming-data/job-parser-data',
         ]
