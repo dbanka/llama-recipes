c0ee1d1b1156d65da8b8ee76fc16e139695157c3
Deepak Banka <deepak.banka@phenompeople.com>
2023-09-05 14:57:00 +0530

    instead of loading from llama config load the model first during ckpts also

--- llama_finetuning.py
+++ llama_finetuning.py
@@ -94,8 +94,8 @@ def main(**kwargs):
     # Load the pre-trained model and setup its configuration
     if train_config.resume_from_checkpoint:
         resume_epoch, resume_step = load_checkpoint_params(train_config)
-        llama_config = LlamaConfig.from_pretrained(train_config.model_path)
-        model = LlamaForCausalLM(llama_config)
+        # llama_config = LlamaConfig.from_pretrained(train_config.model_path)
+        model = LlamaForCausalLM.from_pretrained(train_config.model_path)
         model_checkpoint_found = load_model_checkpoint(model, rank, resume_epoch, resume_step, train_config)
         
     if not model_checkpoint_found:
