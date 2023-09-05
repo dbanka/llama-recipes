508ec035e8a442b4980f96b8fb6115f202713b03
Deepak Banka <deepak.banka@phenompeople.com>
2023-09-05 11:08:51 +0530

    load checkpoint on all the ranks

--- model_checkpointing/checkpoint_handler.py
+++ model_checkpointing/checkpoint_handler.py
@@ -180,13 +180,10 @@ def load_model_checkpoint(model, rank, epoch, step, cfg):
         print(f"model checkpoint not found - {load_full_path} ")
         return False
     
-    if rank == 0:
-        model_checkpoint = torch.load(load_full_path)
-        # integrate into loaded model
-        model.load_state_dict(model_checkpoint)
-        print(f"model checkpoint loaded to rank0 cpu")
-    else:
-        print(f"bypass on rank {rank}")
+    model_checkpoint = torch.load(load_full_path)
+    # integrate into loaded model
+    model.load_state_dict(model_checkpoint)
+    print(f"model checkpoint loaded on rank {rank}")
     return True
 
 
